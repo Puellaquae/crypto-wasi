0056ed34938847b5009fc6f668732ae1734e6d52
Puellaquae <shentukeqin@hotmail.com>
2022-12-22 23:26:03 +0800

    impl: hmac, hkdf, pbkdf2

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,201 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+mod raw;
+
+/// Behaviour like 
+/// 
+/// ```js
+/// let hmac = createHmac(alg, key);
+/// infos.forEach(info => hmac.update(info));
+/// let return = hmac.digest();
+/// ```
+pub fn hmac(
+    alg: &'static str,
+    key: impl AsRef<[u8]>,
+    infos: &[impl AsRef<[u8]>],
+) -> Result<Vec<u8>, raw::CryptoErrno> {
+    let key = key.as_ref();
+    let hmac_alg = match alg {
+        "sha256" | "SHA256" | "HMAC/SHA-256" => "HMAC/SHA-256",
+        "sha512" | "SHA512" | "HMAC/SHA-512" => "HMAC/SHA-512",
+        _ => unreachable!(),
+    };
+    unsafe {
+        let none_opts = raw::OptOptions {
+            tag: raw::OPT_OPTIONS_U_NONE.raw(),
+            u: raw::OptOptionsUnion { none: () },
+        };
+        let hmac_key = raw::symmetric_key_import(hmac_alg, key.as_ptr(), key.len())?;
+        let hmac_handle = raw::symmetric_state_open(
+            hmac_alg,
+            raw::OptSymmetricKey {
+                tag: raw::OPT_SYMMETRIC_KEY_U_SOME.raw(),
+                u: raw::OptSymmetricKeyUnion { some: hmac_key },
+            },
+            none_opts,
+        )?;
+        for info in infos {
+            let info = info.as_ref();
+            raw::symmetric_state_absorb(hmac_handle, info.as_ptr(), info.len())?;
+        }
+        let tag = raw::symmetric_state_squeeze_tag(hmac_handle)?;
+        raw::symmetric_state_close(hmac_handle)?;
+        raw::symmetric_key_close(hmac_key)?;
+        let len = raw::symmetric_tag_len(tag)?;
+        let mut out = vec![0; len];
+        raw::symmetric_tag_pull(tag, out.as_mut_ptr(), out.len())?;
+        raw::symmetric_tag_close(tag)?;
+        Ok(out)
     }
+}
+
+/// As same as `hkdf`, but use hmac to manual expand
+pub fn hkdf_hmac(
+    alg: &'static str,
+    key: impl AsRef<[u8]>,
+    salt: impl AsRef<[u8]>,
+    info: impl AsRef<[u8]>,
+    key_len: usize,
+) -> Result<Vec<u8>, raw::CryptoErrno> {
+    let key = key.as_ref();
+    let salt = salt.as_ref();
+    let info = info.as_ref();
+    let (extract_alg, expand_alg, hash_len) = match alg {
+        "sha256" | "SHA256" => ("HKDF-EXTRACT/SHA-256", "HKDF-EXPAND/SHA-256", 32),
+        "sha512" | "SHA512" => ("HKDF-EXTRACT/SHA-512", "HKDF-EXPAND/SHA-512", 64),
+        _ => return Err(raw::CRYPTO_ERRNO_UNSUPPORTED_ALGORITHM),
+    };
+    let none_opts = raw::OptOptions {
+        tag: raw::OPT_OPTIONS_U_NONE.raw(),
+        u: raw::OptOptionsUnion { none: () },
+    };
+    let expand_key = unsafe {
+        let extract_key = raw::symmetric_key_import(extract_alg, key.as_ptr(), key.len())?;
+        let extract_handle = raw::symmetric_state_open(
+            extract_alg,
+            raw::OptSymmetricKey {
+                tag: raw::OPT_SYMMETRIC_KEY_U_SOME.raw(),
+                u: raw::OptSymmetricKeyUnion { some: extract_key },
+            },
+            none_opts,
+        )?;
+        raw::symmetric_state_absorb(extract_handle, salt.as_ptr(), salt.len())?;
+        let expand_key = raw::symmetric_state_squeeze_key(extract_handle, expand_alg)?;
+        raw::symmetric_state_close(extract_handle)?;
+        let arr_out = raw::symmetric_key_export(expand_key)?;
+        raw::symmetric_key_close(expand_key)?;
+        let len = raw::array_output_len(arr_out)?;
+        let mut buf = vec![0; len];
+        raw::array_output_pull(arr_out, buf.as_mut_ptr(), buf.len())?;
+        Ok(buf)
+    }?;
+    let mut out = vec![0; key_len];
+    let mut last = [].as_slice();
+    for (idx, chunk) in out.chunks_mut(hash_len).enumerate() {
+        let counter = [idx as u8 + 1];
+        chunk.clone_from_slice(&hmac(alg, &expand_key, &[last, info, &counter])?[..chunk.len()]);
+        last = chunk;
+    }
+    Ok(out)
+}
+
+/// Behaviour like `crypto.hkdfSync`
+pub fn hkdf(
+    alg: &'static str,
+    key: impl AsRef<[u8]>,
+    salt: impl AsRef<[u8]>,
+    info: impl AsRef<[u8]>,
+    key_len: usize,
+) -> Result<Vec<u8>, raw::CryptoErrno> {
+    let key = key.as_ref();
+    let salt = salt.as_ref();
+    let info = info.as_ref();
+    let (extract_alg, expand_alg) = match alg {
+        "sha256" | "SHA256" => ("HKDF-EXTRACT/SHA-256", "HKDF-EXPAND/SHA-256"),
+        "sha512" | "SHA512" => ("HKDF-EXTRACT/SHA-512", "HKDF-EXPAND/SHA-512"),
+        _ => return Err(raw::CRYPTO_ERRNO_UNSUPPORTED_ALGORITHM),
+    };
+    let none_opts = raw::OptOptions {
+        tag: raw::OPT_OPTIONS_U_NONE.raw(),
+        u: raw::OptOptionsUnion { none: () },
+    };
+    let mut out = vec![0; key_len];
+    unsafe {
+        let extract_key = raw::symmetric_key_import(extract_alg, key.as_ptr(), key.len())?;
+        let extract_handle = raw::symmetric_state_open(
+            extract_alg,
+            raw::OptSymmetricKey {
+                tag: raw::OPT_SYMMETRIC_KEY_U_SOME.raw(),
+                u: raw::OptSymmetricKeyUnion { some: extract_key },
+            },
+            none_opts,
+        )?;
+        raw::symmetric_state_absorb(extract_handle, salt.as_ptr(), salt.len())?;
+        let expand_key = raw::symmetric_state_squeeze_key(extract_handle, expand_alg)?;
+        raw::symmetric_state_close(extract_handle)?;
+        raw::symmetric_key_close(extract_key)?;
+        let expand_handle = raw::symmetric_state_open(
+            expand_alg,
+            raw::OptSymmetricKey {
+                tag: raw::OPT_SYMMETRIC_KEY_U_SOME.raw(),
+                u: raw::OptSymmetricKeyUnion { some: expand_key },
+            },
+            none_opts,
+        )?;
+        raw::symmetric_state_absorb(expand_handle, info.as_ptr(), info.len())?;
+        raw::symmetric_state_squeeze(expand_handle, out.as_mut_ptr(), out.len())?;
+        raw::symmetric_state_close(expand_handle)?;
+        raw::symmetric_key_close(expand_key)?;
+    }
+    Ok(out)
+}
+
+/// Behaviour like `crypto.pbkdf2Sync`
+pub fn pbkdf2(
+    alg: &'static str,
+    password: impl AsRef<[u8]>,
+    salt: impl AsRef<[u8]>,
+    iters: usize,
+    key_len: usize,
+) -> Result<Vec<u8>, raw::CryptoErrno> {
+    let hash_len = match alg {
+        "sha256" | "SHA256" | "HMAC/SHA-256" => 32,
+        "sha512" | "SHA512" | "HMAC/SHA-512" => 64,
+        _ => return Err(raw::CRYPTO_ERRNO_UNSUPPORTED_ALGORITHM),
+    };
+    let mut out = vec![0; key_len];
+    for (idx, chunk) in out.chunks_mut(hash_len).enumerate() {
+        let mut salt_2 = salt.as_ref().to_vec();
+        let idx = idx + 1;
+        salt_2.push(((idx >> 24) & 0xff) as u8);
+        salt_2.push(((idx >> 16) & 0xff) as u8);
+        salt_2.push(((idx >> 8) & 0xff) as u8);
+        salt_2.push(((idx) & 0xff) as u8);
+        let mut res_t = hmac(alg, password.as_ref(), &[&salt_2])?;
+        let mut res_u = res_t.clone();
+        for _ in 0..iters - 1 {
+            res_u = hmac(alg, password.as_ref(), &[&res_u])?;
+            for k in 0..res_t.len() {
+                res_t[k] ^= res_u[k];
+            }
+        }
+        chunk.copy_from_slice(&res_t[..chunk.len()]);
+    }
+    Ok(out)
+}
+
+/// Convert u8 array to hex string,
+/// behaviour like `Buffer.from(arr).toString("hex")`
+///
+/// # Examples
+///
+/// ```
+/// use crypto_wasi::u8array_to_hex;
+///
+/// assert_eq!(u8array_to_hex([01, 23, 45]), "01172d".to_string());
+/// ```
+pub fn u8array_to_hex(arr: impl AsRef<[u8]>) -> String {
+    arr.as_ref()
+        .iter()
+        .map(|v| format!("{:02x}", v))
+        .collect::<Vec<_>>()
+        .join("")
 }
---
+++ tests/test.rs
@@ -0,0 +1,166 @@
+use crypto_wasi::{hkdf, hkdf_hmac, hmac, pbkdf2, u8array_to_hex};
+
+#[test]
+fn test_hkdf() {
+    let cases256 = [(
+        "sha256",
+        b"secret",
+        b"salt",
+        b"info",
+        64,
+        "f6d2fcc47cb939deafe3853a1e641a27e6924aff7a63d09cb04ccfffbe4776efdda39ae362b1346092d8cb4ee3f0ea74b84000e40066194ffca55d2128aa6c1a",
+    ),
+    (
+        "sha256",
+        b"secret",
+        b"salt",
+        b"info",
+        32,
+        "f6d2fcc47cb939deafe3853a1e641a27e6924aff7a63d09cb04ccfffbe4776ef",
+    )];
+    for (digest, key, salt, info, key_len, execpt) in cases256 {
+        if key_len == 32 {
+            assert_eq!(
+                hkdf(digest, key, salt, info, key_len).map(u8array_to_hex),
+                Ok(execpt.to_string())
+            );
+        }
+        assert_eq!(
+            hkdf_hmac(digest, key, salt, info, key_len).map(u8array_to_hex),
+            Ok(execpt.to_string())
+        );
+    }
+
+    let cases512 = [(
+        "sha512",
+        b"secret",
+        b"salt",
+        b"",
+        128,
+        "683045181e6325bbd2a5ba7fc5cecc3bf0d9bfe0963c3943867cf19c2b5de335faf87a0ad2a75688c78f63dc812a3c5d3ce29ed20ddadaf0edfb985789c66c9077c77c510e1806d3a8affb05d18e3d4c428afcadc968e80c50da7072da4ef446d1c28c6e3facba31809575fe74796cf4fde7238d9cc666b83ec08a0c5125bc9e",
+    ),
+    (
+        "sha512",
+        b"secret",
+        b"salt",
+        b"",
+        64,
+        "683045181e6325bbd2a5ba7fc5cecc3bf0d9bfe0963c3943867cf19c2b5de335faf87a0ad2a75688c78f63dc812a3c5d3ce29ed20ddadaf0edfb985789c66c90",
+    )];
+    for (digest, key, salt, info, key_len, execpt) in cases512 {
+        if key_len == 64 {
+            assert_eq!(
+                hkdf(digest, key, salt, info, key_len).map(u8array_to_hex),
+                Ok(execpt.to_string())
+            );
+        }
+        assert_eq!(
+            hkdf_hmac(digest, key, salt, info, key_len).map(u8array_to_hex),
+            Ok(execpt.to_string())
+        );
+    }
+}
+
+#[test]
+fn test_pbkdf2() {
+    let cases = [
+        (
+            "password",
+            "salt",
+            1,
+            20,
+            "120fb6cffcf8b32c43e7225256c4f837a86548c9",
+        ),
+        (
+            "password",
+            "salt",
+            2,
+            20,
+            "ae4d0c95af6b46d32d0adff928f06dd02a303f8e",
+        ),
+        (
+            "password",
+            "salt",
+            4096,
+            20,
+            "c5e478d59288c841aa530db6845c4c8d962893a0",
+        ),
+        (
+            "passwordPASSWORDpassword",
+            "saltSALTsaltSALTsaltSALTsaltSALTsalt",
+            4096,
+            25,
+            "348c89dbcbd32b2f32d814b8116e84cf2b17347ebc1800181c",
+        ),
+        (
+            "pass\0word",
+            "sa\0lt",
+            4096,
+            16,
+            "89b69d0516f829893c696226650a8687",
+        ),
+        (
+            "password",
+            "salt",
+            32,
+            32,
+            "64c486c55d30d4c5a079b8823b7d7cb37ff0556f537da8410233bcec330ed956",
+        ),
+    ];
+    for (password, salt, iters, key_len, except) in cases {
+        assert_eq!(
+            pbkdf2("sha256", password, salt, iters, key_len).map(u8array_to_hex),
+            Ok(except.to_string())
+        );
+    }
+}
+
+#[test]
+fn test_hmac() {
+    let cases = [
+        (
+            "key",
+            "The quick brown fox jumps over the lazy dog",
+            "f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8",
+        ),
+        (
+            "key",
+            "",
+            "5d5d139563c95b5967b9bd9a8c9b233a9dedb45072794cd232dc1b74832607d0",
+        ),
+        (
+            "",
+            "The quick brown fox jumps over the lazy dog",
+            "fb011e6154a19b9a4c767373c305275a5a69e8b68b0b4c9200c383dced19a416",
+        ),
+        (
+            "",
+            "",
+            "b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad",
+        ),
+    ];
+    for (key, info, except) in cases {
+        assert_eq!(
+            hmac("sha256", key, &[info]).map(u8array_to_hex),
+            Ok(except.to_string())
+        );
+    }
+    assert_eq!(
+        hmac(
+            "sha256",
+            b"\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b",
+            &[b"\x48\x69\x20\x54\x68\x65\x72\x65"]
+        )
+        .map(u8array_to_hex),
+        Ok("b0344c61d8db38535ca8afceaf0bf12b881dc200c9833da726e9376c2e32cff7".to_string())
+    );
+    assert_eq!(
+        hmac(
+            "sha512",
+            b"\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b",
+            &[b"\x48\x69\x20\x54\x68\x65\x72\x65"]
+        )
+        .map(u8array_to_hex),
+        Ok("87aa7cdea5ef619d4ff0b4241a1d6cb02379f4e2ce4ec2787ad0b30545e17cdedaa833b7d6b8a702038b274eaea3f4e4be9d914eeb61f1702e696c203a126854".to_string())
+    );
+}
